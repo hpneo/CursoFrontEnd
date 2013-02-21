56e973698d548e5957c3b0376f03837bb689065e
HPNeo <hpneo@hotmail.com>
2013-02-20 21:09:30 -0500

    Ejemplo Backbone.js : Views

--- bbjs/app.js
+++ bbjs/app.js
@@ -2,6 +2,26 @@ var App = {
   routers : {},
   views : {},
   templates : {},
+  data : {
+    avengers : (function(){
+      var avengers = window.localStorage['avengers'].split(',');
+
+      var list = [];
+
+      for (var i = 0; i < avengers.length; i++) {
+        var avenger_id = avengers[i].toLowerCase().replace(' ', '-');
+        list.push({
+          id : avenger_id,
+          name : avengers[i],
+          description : 'Avenger #' + i,
+          link : "#avengers/" + avenger_id
+        });
+      }
+
+      return list;
+
+    })()
+  },
   init : function() {
     for (r in this.routers) {
       // new App.routers['Avengers']();
@@ -29,15 +49,29 @@ App.views.AvengersIndex = Backbone.View.extend({
       template += item_template.replace('{{avenger_name}}', avenger);
     }
     */
-    var template = "{{#avengers}}\
-    <li>{{.}}</li>\
-    {{/avengers}}";
+    var template = $('#avengers-list-template').text();
 
-    var data = {
-      avengers : window.localStorage['avengers'].split(',')
-    };
+    var output = Mustache.render(template, App.data);
 
-    var output = Mustache.render(template, data);
+    this.el.innerHTML = output;
+
+    return this.el;
+  }
+});
+
+App.views.AvengersShow = Backbone.View.extend({
+  tagName     : 'div',
+  className   : 'avengers-detail',
+  id          : function() {
+    if (this.model) {
+      return this.model.name.toLowerCase().replace(' ', '-');
+    }
+  },
+
+  render      : function() {
+    var template = $('#avengers-detail-template').text();
+
+    var output = Mustache.render(template, this.model);
 
     this.el.innerHTML = output;
 
@@ -56,11 +90,28 @@ App.routers.Avengers = Backbone.Router.extend({
 
     var view = new App.views.AvengersIndex();
 
+    $('#title').text('Avengers');
+
     $('#wrapper').html(view.render());
   },
 
   show : function(id) {
     console.log('avengers#show : ' + id);
+
+    var avenger = null;
+
+    for(a in App.data.avengers) {
+      if (App.data.avengers[a]['id'] == id) {
+        avenger = App.data.avengers[a];
+        break;
+      }
+    }
+
+    var view = new App.views.AvengersShow({
+      model : avenger
+    });
+
+    $('#wrapper').html(view.render());
   }
 
 
